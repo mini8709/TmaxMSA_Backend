6a05f71b58cd96d7a97055879ef78bdd3bbd6774
FreeCoderKR <87847630+FreeCoderKR@users.noreply.github.com>
2021-09-10 12:41:54 +0900

    Swagger Add Review Edit (#9)

---
+++ src/main/java/com/msa/server/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.msa.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    private static final String API_NAME = "TMAX_MSA API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "티맥스 엔터프라이즈 MSA TF";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.msa.server.adapter"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .build();
+    }
+
+}
